0ab74e6367c306c31bb36bc2d44b32e6be2df38d
Vincent Hoogendoorn <vincenth@workstreampeople.com>
2020-05-26 12:05:34 +0200

    Cleanup and document markup helpers

--- sample/System.Maui.Samples/Markup/Helpers.cs
+++ sample/System.Maui.Samples/Markup/Helpers.cs
@@ -1,5 +1,9 @@
 namespace System.Maui.Samples.Markup
 {
+	/// <summary>
+	/// Factory eliminates new keyword and Children = { } initializers from markup.
+	/// Users can create custom factories;select which factory to use with e.g. <code>using static System.Maui.Markup.Factory;</code>
+	/// </summary>
 	public static class Factory
 	{
 		public static Label Label(Binding<string> value = null) => new Label(value);
@@ -14,33 +18,37 @@ namespace System.Maui.Samples.Markup
 		}
 	}
 
+	/// <summary>This class illustrates several alternate strategies for reducing noise in markup</summary>
 	public static class MarkupExtensions
 	{
-
-		// Generate fluid method for each enum value / class static constant:
-		public static T Color_Black<T>(this T view) where T : View => view.Color(Maui.Color.Black);
-		public static T Color_White<T>(this T view) where T : View => view.Color(Maui.Color.White);
-
-		public static T Background_Black<T>(this T view) where T : View => view.Background(Maui.Color.Black);
-		public static T Background_White<T>(this T view) where T : View => view.Background(Maui.Color.White);
-
-		// Regular helpers from parameter ctor overloads:
-		public static T Color<T>(this T view, string colorAsHex) where T : View => view.Color(new Color(colorAsHex));
-		public static T Color<T>(this T view, float red, float green, float blue) where T : View => view.Color(new Color(red, green, blue));
-		public static T Color<T>(this T view, float red, float green, float blue, float alpha) where T : View => view.Color(new Color(red, green, blue, alpha));
-
-		// Eliminate helper name, use parameter overload
+		// Combine two helpers into one:
 		public static T Color<T>(this T view, Color color, Color background) where T : View => view.Color(color).Background(background);
 		public static T Color<T>(this T view, string color, string background) where T : View => view.Color(color).Background(background);
 		public static T Color<T>(this T view, Color color, string background) where T : View => view.Color(color).Background(background);
 		public static T Color<T>(this T view, string color, Color background) where T : View => view.Color(color).Background(background);
 
+		public static T Font<T>(this T view, float size, string family) where T : Label => view.FontSize(size).FontFamily(family);
+
+		// Eliminate instance value type name repetition: .Color(new Color("#ABCDEF")) -> .Color("#ABCDEF") by generating helpers from parameter ctor overloads:
+		public static T Color<T>(this T view, string colorAsHex) where T : View => view.Color(new Color(colorAsHex));
+		public static T Color<T>(this T view, float red, float green, float blue) where T : View => view.Color(new Color(red, green, blue));
+		public static T Color<T>(this T view, float red, float green, float blue, float alpha) where T : View => view.Color(new Color(red, green, blue, alpha));
+
+		// Eliminate static value enum/type name repetition: .TextAlignment(TextAlignment.Natural) -> .TextNatural() by generating a fluid method for each enum value / class static constant:
 		public static T TextNatural  <T>(this T view) where T : Label => view.TextAlignment(Maui.TextAlignment.Natural  );
 		public static T TextLeft     <T>(this T view) where T : Label => view.TextAlignment(Maui.TextAlignment.Left     );
 		public static T TextRight    <T>(this T view) where T : Label => view.TextAlignment(Maui.TextAlignment.Right    );
 		public static T TextCenter   <T>(this T view) where T : Label => view.TextAlignment(Maui.TextAlignment.Center   );
 		public static T TextJustified<T>(this T view) where T : Label => view.TextAlignment(Maui.TextAlignment.Justified);
 
+		public static T LinesNoWrap<T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.NoWrap);
+		public static T LinesWordWrap<T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.WordWrap);
+		public static T LinesCharacterWrap<T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.CharacterWrap);
+		public static T LinesHeadTruncation<T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.HeadTruncation);
+		public static T LinesTailTruncation<T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.TailTruncation);
+		public static T LinesMiddleTruncation<T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.MiddleTruncation);
+
+		// Equivalent to above helpers but with subchain so .Text().Natural() instead of .TextNatural()
 		public static Subchain<TLabel, TextAlignment> Text<TLabel>(this TLabel label) where TLabel : Label => new Subchain<TLabel, TextAlignment>(label);
 		public static TLabel Natural  <TLabel>(this Subchain<TLabel, TextAlignment> subchain) where TLabel : Label => subchain.Parent.TextAlignment(Maui.TextAlignment.Natural  );
 		public static TLabel Left     <TLabel>(this Subchain<TLabel, TextAlignment> subchain) where TLabel : Label => subchain.Parent.TextAlignment(Maui.TextAlignment.Left     );
@@ -48,6 +56,7 @@ namespace System.Maui.Samples.Markup
 		public static TLabel Center   <TLabel>(this Subchain<TLabel, TextAlignment> subchain) where TLabel : Label => subchain.Parent.TextAlignment(Maui.TextAlignment.Center   );
 		public static TLabel Justified<TLabel>(this Subchain<TLabel, TextAlignment> subchain) where TLabel : Label => subchain.Parent.TextAlignment(Maui.TextAlignment.Justified);
 
+		// Equivalent to above helpers but with subchain so .Lines().NoWrap() instead of .LinesNoWrap()
 		public static Subchain<TLabel, LineBreakMode> Lines<TLabel>(this TLabel label) where TLabel : Label => new Subchain<TLabel, LineBreakMode>(label);
 		public static TLabel NoWrap          <TLabel>(this Subchain<TLabel, LineBreakMode> subchain) where TLabel : Label => subchain.Parent.LineBreakMode(Maui.LineBreakMode.NoWrap          );
 		public static TLabel WordWrap        <TLabel>(this Subchain<TLabel, LineBreakMode> subchain) where TLabel : Label => subchain.Parent.LineBreakMode(Maui.LineBreakMode.WordWrap        );
@@ -55,19 +64,9 @@ namespace System.Maui.Samples.Markup
 		public static TLabel HeadTruncation  <TLabel>(this Subchain<TLabel, LineBreakMode> subchain) where TLabel : Label => subchain.Parent.LineBreakMode(Maui.LineBreakMode.HeadTruncation  );
 		public static TLabel TailTruncation  <TLabel>(this Subchain<TLabel, LineBreakMode> subchain) where TLabel : Label => subchain.Parent.LineBreakMode(Maui.LineBreakMode.TailTruncation  );
 		public static TLabel MiddleTruncation<TLabel>(this Subchain<TLabel, LineBreakMode> subchain) where TLabel : Label => subchain.Parent.LineBreakMode(Maui.LineBreakMode.MiddleTruncation);
-
-		public static T LinesNoWrap<T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.NoWrap          );
-		public static T LinesWordWrap        <T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.WordWrap        );
-		public static T LinesCharacterWrap   <T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.CharacterWrap   );
-		public static T LinesHeadTruncation  <T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.HeadTruncation  );
-		public static T LinesTailTruncation  <T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.TailTruncation  );
-		public static T LinesMiddleTruncation<T>(this T view) where T : Label => view.LineBreakMode(Maui.LineBreakMode.MiddleTruncation);
-
-		public static T Font<T>(this T view, float size, string family) where T : Label => view.FontSize(size).FontFamily(family);
 	}
 
-	// TODO: subchains is probably a standard pattern with fluent api's? -> look it up and align
-	public struct Subchain<TParent, TChild> // struct to prevent adding GC pressure
+	public class Subchain<TParent, TChild> where TParent : class
 	{
 		public readonly TParent Parent;
 		public Subchain(TParent parent) => Parent = parent;
